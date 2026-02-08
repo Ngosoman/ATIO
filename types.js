ea1dc01dae229ce4638bd1dffb19429c9a7d778a
Tom Mwambingu <[tomchom30@gmail.com]>
2026-02-08 13:15:52 +0300

    Implemented a types file to add a type function

---
+++ types.js
@@ -0,0 +1,29 @@
+
+export const UserPersona = {
+  POLICY_MAKER: "Policy Maker",
+  NGO: "NGO",
+  EXTENSION_OFFICER: "Extension Officer",
+  FARMER_ORGANIZATION: "Farmer Organization",
+  PRIVATE_SECTOR: "Private Sector"
+};
+
+export const ProblemArea = {
+  POST_HARVEST_LOSS: "Post-harvest loss",
+  WATER_SCARCITY: "Water scarcity",
+  CLIMATE_RESILIENCE: "Climate resilience",
+  WOMEN_LABOR_BURDEN: "Women labor burden",
+  PRODUCTIVITY_GAPS: "Productivity gaps"
+};
+
+export const Level = {
+  LOW: "Low",
+  MEDIUM: "Medium",
+  HIGH: "High"
+};
+
+export const DecisionGoal = {
+  PILOT: "Pilot innovation",
+  SCALE: "Scale innovation",
+  INFORM_POLICY: "Inform policy",
+  TRAINING: "Training & extension"
+};
